78238a37c93434cc11c94dcc5c82d8f489aaaaa1
Asaf Silman <asafsilman@gmail.com>
2018-04-27 16:07:26 +0800

    Renamed cubes

--- DataCube1.sql
+++ DataCube1.sql
@@ -1,6 +1,6 @@
 use mydb;
-select d.Date,p.ProductName,w.WarehouseName, AVG(fs.StockLevel) AverageStock from FactStockLevels fs
-right outer join DimWarehouse w on fs.WarehouseID=w.WarehouseId
-right outer join DimDate d on fs.DateID=d.DateID
-right outer join DimProduct p on fs.ProductId=p.ProductID
-GROUP BY CUBE(d.Date, p.ProductName, w.WarehouseName);
+select d.Date,p.ProductName,g.EnglishCountryRegionName, SUM(o.Quantity) TotalQuantitySold from FactOrderFulfilment o
+right outer join DimGeography g on o.GeographyKey=g.GeographyId
+right outer join DimDate d on o.OrderDate=d.DateID
+right outer join DimProduct p on o.ProductKey=p.ProductID
+GROUP BY CUBE(d.Date, p.ProductName, g.EnglishCountryRegionName);
